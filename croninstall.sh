bfe1355ea7af9e2ea88e32e888265e6cfd177f11
Jacob Lane <jaclane299@gmail.com>
2015-07-30 18:47:23 -0500

    Update croninstall.sh

--- croninstall.sh
+++ croninstall.sh
@@ -3,5 +3,6 @@ echo "Preparing Cron Directory";
 busybox mount -o remount,rw -t auto /system 2>/dev/null;
 mkdir /system/etc/cron.d 2>/dev/null;
 echo "Preparing Cron Trigger";
+cat /tmp/01cron > /system/etc/init.d/01cron;
 chmod 755 /system/etc/init.d/01cron;
 exit 0
